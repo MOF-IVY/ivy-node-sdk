cfc1cfbb0362d59b947de4a59be3663e22142c32
Caius Citiriga <nbkmaxine@gmail.com>
2023-09-27 01:13:21 +0200

    added OHLCV positions

--- src/models/common/ohlcv.type.ts
+++ src/models/common/ohlcv.type.ts
@@ -1,1 +1,32 @@
+export enum OHLCVPositions {
+  TIME = 0,
+  OPEN = 1,
+  HIGH = 2,
+  LOW = 3,
+  CLOSE = 4,
+  VOLUME = 5,
+  TRADE_TIME = 6,
+}
+
+/**
+ * Used for final kline events
+ *
+ * Timestamp
+ * Open
+ * High
+ * Low
+ * Close
+ */
 export type OHLCV = [number, number, number, number, number, number];
+
+/**
+ * Used for intra kline events:
+ *
+ * Timestamp
+ * Open
+ * High
+ * Low
+ * Close
+ * Trade Time
+ */
+export type OHLCVT = [number, number, number, number, number, number, number];
